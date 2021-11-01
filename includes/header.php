16b3d5f653a95ad5bb2920cac078c2cd9d9691ef
luqaska <83518257+luqaska@users.noreply.github.com>
2021-11-01 09:31:57 -0300

    Initial commit

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="<?php echo L::lang ?>" dir="ltr">
+  <head>
+    <title><?php echo $title ?> | Frifoss</title>
+    <link rel="stylesheet" href="css/styles.css" />
+    <link rel="icon" href="https://midi.moe/f/neabj.png" />
+    <meta name="viewport" content="width=device-width" />
+  </head>
+  <body>
+    <header><a href="/" style="color:black;text-decoration:none"><h1><img src="https://midi.moe/f/neabj.png" width="30px" height="auto" /> Frifoss</h1></a><p></p></header>
+    <nav><p><a href="https://app.7ort.rf.gd"><?php echo L::go_to_7ort ?></a> - <a href="credits.php"><?php echo L::credits_credits ?></a> - <a href="https://bl.7ort.rf.gd/?b=frifoss"><?php echo L::news ?></a></p></nav>
---
+++ includes/start.php
@@ -0,0 +1,1 @@
+<?php include 'includes/i18n.class.php';$i18n=new i18n();$i18n->setCachePath('cache');$i18n->init(); ?>
